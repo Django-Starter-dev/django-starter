f5f04ad21c4f06a2dfd8d3ac61b9747a17e9f67d
surajbalwani <surajbalwani.itm@gmail.com>
2022-08-22 05:32:45 -0700

    changes made for part-7.

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,36 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+
+## ----------- changing the sequence of the fields in the model ------------ ##
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+## ------------------------------------------------------------------------- ##
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
     
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -10,6 +11,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
     def was_published_recently(self):
         print("debug text")
         print(self.pub_date)
@@ -18,6 +20,10 @@ class Question(models.Model):
         
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
